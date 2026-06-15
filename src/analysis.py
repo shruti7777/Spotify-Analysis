8c5e8b9f1e6fe113b936c270719a8a07e2615240
Shruti Sarkar <antaraludul123@gmail.com>
2026-06-15 09:52:54 +0530

    data cleaning done

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,9 @@
 import pandas as pd
 df= pd.read_csv("data/spotifysongs.csv")
 print(df.head())
-print (df.columns)
+print (df.info())
+print(df.isnull().sum())
+df = df.dropna()
+print(df.duplicated().sum())
+df = df.drop_duplicates()
+
